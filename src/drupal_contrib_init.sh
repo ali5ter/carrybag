901a6921c4d8abc723955e89c9d1277f3c3ec6a8
Alister Lewis-Bowen <alister@different.com>
2008-02-16 17:50:05 +0000

    seperate out the module list

--- src/drupal_contrib_init.sh
+++ src/drupal_contrib_init.sh
@@ -4,62 +4,14 @@
 # Usage: drupal_contrib_init [core_version]
 # where core_version is 4, 5, 6 or HEAD (default)
 #       version is an integer, defaulting to 1
+# Modules are listed by project name, one per line, in drupal_contrib_module.list
 # @author Alister Lewis-Bowen (alister@different.com)
 
 BRANCH=${1:-HEAD}; # default to HEAD
 
-for module in actions \
-	ajaxim \
-	asset \
-	backup_migrate \
-	biblio \
-	buddylist \
-	cck \
-	coder \
-	contemplate \
-	custom_breadcrumbs \
-	date \
-	devel \
-	event \
-	forms \
-	google_analytics \
-	image \
-	imagecache \
-	imagefield \
-	imce \
-	jstools \
-	link \
-	location \
-	logintoboggan \
-	mapi \
-	mediafield \
-	menu_per_role \
-	menutrails \
-	node_breadcrumb \
-	node_media \
-	nodeaccess \
-	nodehierarchy \
-	og \
-	override_node_options \
-	panels \
-	pathauto \
-	pictures \
-	pngfix \
-	profileplus \
-	services \
-	similarterms \
-	simplemenu \
-	simpletest \
-	suggestedterms \
-	survey \
-	tinymce \
-	token \
-	typogrify \
-	update_status \
-	video views \
-	views_bonus \
-	workflow \
-	workflow_ng; do
+MODULE_LIST=`dirname $0`/../etc/drupal_contrib_module.list
+
+for module in `cat $MODULE_LIST`; do
 	
 	drupal_contrib_co.sh $module $BRANCH;
 done;
