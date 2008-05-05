6375307ca0995514c74dee08dc8210b461c91f9a
Alister Lewis-Bowen <alister@different.com>
2008-05-05 18:01:50 +0000

    automatically look for the newest version

--- bin/drupal_module_co.sh
+++ bin/drupal_module_co.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
+# ----------------------------------------------------------------------------
 # @file
 # Check out a version of a Drupal contrib project direct from CVS
 # @see color
 # @author Alister Lewis-Bowen (alister@different.com)
+# ----------------------------------------------------------------------------
 
 PROJECT=$1;
 BRANCH=$2;
-VERSION=${3:-1};
-
+VERSION=$3;
+REPO='-d:pserver:anonymous:anonymous@cvs.drupal.org:/cvs/drupal-contrib';
+REPODIR=contributions/modules/;
 TAG=HEAD;
+LOG=/tmp/`basename $0`.log;
+
+# Function: Help
+# ----------------------------------------------------------------------------
 
 function help {
 	echo;
-	echo "Usage: $(color bd)drupal_module_co.sh project$(color off) [$(color bd)branch$(color off)] [$(color bd)version$(color off)]";
-	echo "where $(color bd)project$(color off) is the name of the module project,";
-	echo "      $(color bd)branch$(color off) is 4, 5, 6, or HEAD (default), ";
-	echo "      $(color bd)version$(color off) is an integer (defaults to 1)";
+	echo "Usage: $(color bd)drupal_module_co.sh project$(color) [$(color ul)branch$(color)] [$(color ul)version$(color)]";
+	echo "where $(color ul)project$(color) is the name of the module project,";
+	echo "      $(color ul)branch$(color) is 4, 5, 6, or HEAD (default), ";
+	echo "      $(color ul)version$(color) is an integer (defaults to the latest version)";
 	echo;
 	exit 1;
 }
+
+# Parse input arguments
+# ----------------------------------------------------------------------------
 
 if [[ -z $PROJECT || "$PROJECT" = '-h' || "$PROJECT" = '--help' ]]; then help; fi;
 
@@ -37,13 +47,103 @@ case $BRANCH in
 		;;
 esac;
 
-if [ ! -z $3 ]; then TAG="$TAG--$VERSION"; fi
 
-if [[ "$BRANCH" = '6' && -z $VERSION ]]; then TAG="$TAG--$VERSION"; fi # New tag system in branch 6 (sort of in 5 but came out foo)
+# Check if the project dir exists
+# ----------------------------------------------------------------------------
+
+if [ -d $PROJECT ]; then
+	echo "$(color yellow)This project has already been checked out.$(color)";
+	exit 1;
+fi;
+
+# Check out project module from the DRUPAL contrib CVS 
+# ----------------------------------------------------------------------------
+
+echo -n "Fetching $(color white blue) $PROJECT $(color) module from Drupal Contrib CVS ";
+
+if [ "$VERSION" ]; then
 	
-echo -n "$(color bd)Fetching $(color white blue)$(color bd)$PROJECT$(color off) $(color bd)module from Drupal Contrib CVS ($(color yellow)$TAG$(color off)$(color bd))...$(color off)";
-cvs -d:pserver:anonymous:anonymous@cvs.drupal.org:/cvs/drupal-contrib checkout -d $PROJECT -r $TAG contributions/modules/$PROJECT >/tmp/`basename $0`.log 2>&1;
+	# Check out a specific version...
+	
+	TAG="$TAG--$VERSION";
+	echo -n " ($(color yellow)$TAG$(color))...";
+	cvs $REPO -Q co -d $PROJECT -r $TAG $REPODIR$PROJECT
+	if [ ! -d $PROJECT ]; then
+		echo " $(color red)Unable to checkout this project module.$(color)";
+		exit 1;
+	fi;
+	
+else
 
-echo " $(color green)Done$(color off)";
+	# Find latest version...
+	
+	CHECKOUT_VERSION_TAG=0
+	CHECKOUT_TAG=1
+	CHECKOUT_HEAD=2
+	PROJECT_NOT_FOUND=;
+	TAG_NOT_FOUND=;
+	EXIT=NO;
+	TRIES=0;
+	
+	if [ "$TAG" == 'HEAD' ]; then CHECKOUT=$CHECKOUT_HEAD;
+	else CHECKOUT=$CHECKOUT_VERSION_TAG;
+	fi;
+	
+	until [[ -d $PROJECT || "$EXIT" == 'YES' ]]; do
+	
+		case "$CHECKOUT" in
+		
+			"$CHECKOUT_VERSION_TAG" )
+				if [ -z "$_TAG" ]; then
+					VERSION=9;
+					_TAG=$TAG--$VERSION;
+				fi;
+				if (("$VERSION" > "1")); then
+					VERSION=$[$VERSION -1];
+					_TAG=$TAG--$VERSION;
+				else 
+					CHECKOUT=$CHECKOUT_TAG;
+				fi;
+				;;
+				
+			"$CHECKOUT_TAG" )
+				if [ "$_TAG" == "$TAG" ]; then
+					CHECKOUT=$CHECKOUT_HEAD;
+				else
+					_TAG=$TAG;
+				fi;
+				;;
+				
+			"$CHECKOUT_HEAD" )
+			  if [ "$_TAG" == 'HEAD' ]; then
+					EXIT=YES;
+				else
+					_TAG=HEAD;
+				fi;
+			  ;;
+			  
+			* )
+				EXIT=YES;
+				;;
+		esac;
+		
+		cvs $REPO -Q co -d $PROJECT -r $_TAG $REPODIR$PROJECT 2>$LOG;
+		
+		PROJECT_NOT_FOUND=`cat $LOG | grep 'could not read RCS file'`;
+		TAG_NOT_FOUND=`cat $LOG | grep 'no such tag'`;
+		
+		echo -n '.';
+		
+	done;
+
+	if [ "$PROJECT_NOT_FOUND" != '' ]; then
+		echo " $(color red)Unable to find this project module.$(color)";
+		exit 1;
+	fi;
+	
+	echo -n " ($(color yellow)$_TAG$(color))";
+fi;
+
+echo " $(color green)Done$(color)";
 
 exit 0;
