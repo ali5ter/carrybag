f1f88b8e4bc55c2960d3ab9e4586d485a4efc777
Alister Lewis-Bowen <alister@different.com>
2015-02-25 08:02:54 -0500

    tried it, didn't like it - take out 'vim .' alias

--- plugins/zzz-carrybag-overrides.plugin.bash
+++ plugins/zzz-carrybag-overrides.plugin.bash
@@ -1,8 +1,5 @@
 cite about-alias
 about-alias 'CarryBag modifications to override any Bash-it addons'
 
-## Preferred vim start alias
-unalias v && alias vi='vim .'
-
 ## Preferred git aliases
 unalias gd && alias gd='git diff'
