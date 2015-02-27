fbc546968b41c765c2154f44da5a45f9ee075deb
Alister Lewis-Bowen <alister@different.com>
2015-02-27 11:16:21 -0500

    comments

--- lib/runcom.bash
+++ lib/runcom.bash
@@ -33,20 +33,42 @@ _build_carrybag_bash_runcom () {
 
     local BASHRC=$(_bash_runcom)
 
+    ## Use Bash-it runcom as a template
     [ -w "$BASHRC" ] && cp "$BASHRC" "$BASHRC.bak" &&
         echo -e "${echo_cyan}Your $(basename "$BASHRC") has been backed up to $BASHRC.bak${echo_normal}"
     cp "$BASH_IT/template/bash_profile.template.bash" "$BASHRC"
 
+    ## Persist the OS specific bash runcom name
     _add_to_bash_runcom "export BASHRC=\'$BASHRC\'"
+
+    ## Include man pages added by package managers
     _add_to_bash_runcom "export MANPATH=\'/usr/local/man:$MANPATH\'"
+
+    ## Use the Bash-it theme provided by CarryBag
     _add_to_bash_runcom "export BASH_IT_THEME='alister'"
+
+    ## Make our default editor vim
     _add_to_bash_runcom "export EDITOR='vim'"
-    _add_to_bash_runcom "export GIT_EDITOR='vim'"
+
+    ## Make sure git uses the same edtor
+    _add_to_bash_runcom "export GIT_EDITOR=\'$EDITOR\'"
+
+    ## Use vim to edit on the cmdl
     _add_to_bash_runcom "set -o vi"
+
+    ## Avoid storing duplicate command and those starting with spaces
     _add_to_bash_runcom "HISTCONTROL=ignoreboth"
+
+    ## Help correct minor typos when changing directories
     _add_to_bash_runcom "shopt -s cdspell"
+
+    ## Append commands to the history instead of overwriting when exiting
     _add_to_bash_runcom "shopt -s histappend"
+
+    ## Include dot files in pathname expansion (globbing)
     _add_to_bash_runcom "shopt -s dotglob"
+
+    ## Update window size vars after each command
     _add_to_bash_runcom "shopt -s checkwinsize"
 
     echo -e "${echo_cyan}CarryBag modifications have been applied to $(basename "$BASHRC")${echo_normal}"
