797859103b54311298f1a401a9f2cfc93d34cd91
Alister Lewis-Bowen <alister@different.com>
2015-03-19 12:55:41 -0400

    override GREP color

--- plugins/zzz-carrybag-overrides.plugin.bash
+++ plugins/zzz-carrybag-overrides.plugin.bash
@@ -1,5 +1,8 @@
 cite about-alias
 about-alias 'CarryBag modifications to override any Bash-it addons'
 
+## Override GREP coloring
+export GREP_COLOR='0;97'    ## normal; white works well with Solarized Dark
+
 ## Preferred git aliases
 unalias gd && alias gd='git diff'
