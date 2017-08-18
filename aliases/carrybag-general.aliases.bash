01a7f6350c35898df7683c664652919ec7017fba
Alister Lewis-Bowen <bowen@vmware.com>
2017-08-18 10:05:54 -0400

    fix placement of carrybag logs

--- aliases/carrybag-general.aliases.bash
+++ aliases/carrybag-general.aliases.bash
@@ -5,8 +5,8 @@ about-alias 'CarryBag general aliases'
 alias sourcep='source "$BASHRC"'
 
 ## CarryBag bootstrap and housekeeping
-alias bootstrap='_PWD=$PWD; cd $CB_BASE; cb_bootstrap 2>&1 | tee bootstrap.log; cd $_PWD'
-alias housekeeping='_PWD=$PWD; cd $CB_BASE; cb_housekeeping 2>&1 | tee housekeeping.log; cd $_PWD'
+alias bootstrap='_PWD=$PWD; cd $CB_BASE; cb_bootstrap 2>&1 | tee ~/.cb_bootstrap.log; cd $_PWD'
+alias housekeeping='_PWD=$PWD; cd $CB_BASE; cb_housekeeping 2>&1 | tee ~/.cb_housekeeping.log; cd $_PWD'
 
 ## Tail the aggregation of log files
 alias allogs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f"
